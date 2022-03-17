c58c92bcc369aa457472af55586adfe57fa7f13b
Arthur Gomes Valadão <arthur.valadao@itriad.org.br>
2022-03-17 17:57:13 -0400

    archive .env create

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,17 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
-import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyBozVXSd4GR86zH_-T_4SIKAAq59jac_yI",
-   authDomain: "letmeask-5cbc5.firebaseapp.com",
-   databaseURL: "https://letmeask-5cbc5-default-rtdb.firebaseio.com",
-   projectId: "letmeask-5cbc5",
-   storageBucket: "letmeask-5cbc5.appspot.com",
-   messagingSenderId: "274608646388",
-   appId: "1:274608646388:web:ad2092cbb79e1321f696a1"
+   apiKey: process.env.REACT_APP_API_KEY,
+   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+   databaseURL: process.env.REACT_APP_DATABASE_URL,
+   projectId: process.env.REACT_APP_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig); 
