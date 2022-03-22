14257fc266ab82e9b2d844f70a0ffe8cc744fa3e
Arthur <Arth.valadao@gmail.com>
2022-03-22 15:43:45 -0400

    Auth apply stylish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,34 +2,36 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIcon from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
-
-        <main>
-          <div>
-            <img src={logoImg} alt="Letmeask" />
-            <button>
-              <img src={googleIcon} alt="Logo do Google" />
-              Crie sua sala com o Google
-            </button>
-            <div>ou entre em uma sala </div>
-            <form>
-              <input
-                type="text"
-                placeholder="Digite o código da sala"
-              />
-              <button type="submit">
-                Entrar na sala
-              </button>
-            </form>
-          </div>
-        </main>
       </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button className="create-room">
+            <img src={googleIcon} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala </div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button type="submit">
+              Entrar na sala
+            </button>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
